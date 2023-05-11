536a7f114f03e94607ffe0162993f8cb64f0f2e1
Emrullah10 <kemalbozkurt1006@gmail.com>
2023-05-11 16:06:22 +0300

    burc_Application

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.pink),
+      theme: ThemeData(primarySwatch: Colors.orange),
       home: BurcListesi(),
     );
   }
